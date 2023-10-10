0d0d6b532e01cfc553404ace1637c2ef15f20d2b
Charro Gruver <null>
2023-10-10 12:11:17 +0000

    add keycloak init

--- dev-init/keycloak.sh
+++ dev-init/keycloak.sh
@@ -1,9 +1,12 @@
+#!/usr/bin/env bash
 
+KCADM=/opt/keycloak/bin/kcadm.sh
+KCADM_CONFIG=/tmp/kcadm.config
 
-/opt/keycloak/bin/kcadm.sh config credentials --server http://localhost:8081 --realm master --user admin --password admin
-/opt/keycloak/bin/kcadm.sh create realms -s realm=pelorus -s enabled=true -o
-/opt/keycloak/bin/kcadm.sh create users -r pelorus -s username=pelorus-metrics -s enabled=true
-./kcadm.sh set-password -r pelorus --username pelorus-metrics --new-password pelorus
-./kcadm.sh create clients -r pelorus -s clientId=pelorus-client -s publicClient="true"  -s "redirectUris=[\"http://localhost:8080/*\"]" -s enabled=true
-./kcadm.sh create roles -r pelorus -s name=pelorus-metrics
-./kcadm.sh add-roles --uusername pelorus-metrics --rolename pelorus-metrics -r pelorus
+${KCADM} config credentials --server http://localhost:8081 --realm master --user admin --password admin --config ${KCADM_CONFIG} 
+${KCADM} create realms -s realm=pelorus -s enabled=true -o --config ${KCADM_CONFIG} 
+${KCADM} create users -r pelorus -s username=pelorus-metrics -s enabled=true --config ${KCADM_CONFIG} 
+${KCADM} set-password -r pelorus --username pelorus-metrics --new-password pelorus --config ${KCADM_CONFIG} 
+${KCADM} create clients -r pelorus -s clientId=pelorus-client -s publicClient="true"  -s "redirectUris=[\"http://localhost:8080/*\"]" -s enabled=true --config ${KCADM_CONFIG} 
+${KCADM} create roles -r pelorus -s name=pelorus-metrics --config ${KCADM_CONFIG} 
+${KCADM} add-roles --uusername pelorus-metrics --rolename pelorus-metrics -r pelorus --config ${KCADM_CONFIG} 
