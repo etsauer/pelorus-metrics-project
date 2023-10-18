345d6f4cdb939ced23faefc62b19ca2b84115f11
Charro Gruver <null>
2023-10-18 19:44:13 +0000

    update Next env for client

--- dev-init/env-init.sh
+++ dev-init/env-init.sh
@@ -24,12 +24,12 @@ fi
 
 APP_URL=https://$(${OC} --kubeconfig=/home/user/.kube/config get route ${DEVWORKSPACE_ID}-dev-tools-3000-pelorus-metrics -o jsonpath={.spec.host})
 KEYCLOAK_URL=https://$(${OC} --kubeconfig=/home/user/.kube/config get route ${DEVWORKSPACE_ID}-keycloak-8081-keycloak -o jsonpath={.spec.host})
-PELORUS_API_URL=https://$(${OC} --kubeconfig=/home/user/.kube/config get route ${DEVWORKSPACE_ID}-dev-tools-8080-pelorus-api -o jsonpath={.spec.host})
+NEXT_PUBLIC_PELORUS_API_URL=https://$(${OC} --kubeconfig=/home/user/.kube/config get route ${DEVWORKSPACE_ID}-dev-tools-8080-pelorus-api -o jsonpath={.spec.host})
 ${OC} --kubeconfig=/home/user/.kube/config whoami -t > /tmp/token
 
 echo "export NEXTAUTH_URL=${APP_URL}" >> ${PROJECTS_ROOT}/env-tmp/set-env.sh
 echo "export APP_ROOT=${APP_URL}" >> ${PROJECTS_ROOT}/env-tmp/set-env.sh
-echo "export PELORUS_API_URL=${PELORUS_API_URL}" >> ${PROJECTS_ROOT}/env-tmp/set-env.sh
+echo "export NEXT_PUBLIC_PELORUS_API_URL=${NEXT_PUBLIC_PELORUS_API_URL}" >> ${PROJECTS_ROOT}/env-tmp/set-env.sh
 echo "export NEXTAUTH_SECRET=\"+03/pWr4sFvxrcm29hwnXLREWjFfmW2toTCUyNcLytc=\"" >> ${PROJECTS_ROOT}/env-tmp/set-env.sh
 echo "export KEYCLOAK_ISSUER=${KEYCLOAK_URL}/realms/pelorus" >> ${PROJECTS_ROOT}/env-tmp/set-env.sh
 echo "export KEYCLOAK_ID=pelorus-client" >> ${PROJECTS_ROOT}/env-tmp/set-env.sh
@@ -38,3 +38,5 @@ echo "export KCADM=/opt/keycloak/bin/kcadm.sh" >> ${PROJECTS_ROOT}/env-tmp/set-e
 echo "export KCADM_CONFIG=/tmp/kcadm.config" >> ${PROJECTS_ROOT}/env-tmp/set-env.sh
 echo "export AUTH_TOKEN=/tmp/token" >> ${PROJECTS_ROOT}/env-tmp/set-env.sh
 echo "export PELORUS_URL=https://prometheus-pelorus.pelorus.svc:9091/api/v1" >> ${PROJECTS_ROOT}/env-tmp/set-env.sh
+echo "export CORS_ORIGINS=\"/.*/\"" >> ${PROJECTS_ROOT}/env-tmp/set-env.sh
+echo "export CORS_METHODS=GET" >> ${PROJECTS_ROOT}/env-tmp/set-env.sh
